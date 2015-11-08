d2e459f0922be30836d607f06fefa9c032cbb594
Tino Langer <Tino@tinosmacbook.fritz.box>
2015-11-08 21:21:03 +0100

    Initial Commit

---
+++ e3-konkret WatchKit Extension/ExtensionDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  ExtensionDelegate.swift
+//  e3-konkret WatchKit Extension
+//
+//  Created by Tino Langer on 08.11.15.
+//  Copyright © 2015 Tino Langer. All rights reserved.
+//
+
+import WatchKit
+
+class ExtensionDelegate: NSObject, WKExtensionDelegate {
+
+    func applicationDidFinishLaunching() {
+        // Perform any final initialization of your application.
+    }
+
+    func applicationDidBecomeActive() {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillResignActive() {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, etc.
+    }
+
+}
---
+++ e3-konkret WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  e3-konkret WatchKit Extension
+//
+//  Created by Tino Langer on 08.11.15.
+//  Copyright © 2015 Tino Langer. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
